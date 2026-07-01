6b626ad8d4ebffead7fa17e0facf86e4ac0be384
emon233 <sazid.hossain.1433@gmail.com>
2026-07-01 16:41:14 +0600

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,16 +11,16 @@ module.exports = {
       autorestart: true,
       max_memory_restart: '512M',
     },
-    {
-      name: 'blockchain-tron',
-      cwd: '/var/www/vhosts/1usdt.game/blockchain-network',
-      script: 'tron.js',
-      env: {
-        NODE_ENV: 'production',
-      },
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '512M',
-    },
+    // {
+    //   name: 'blockchain-tron',
+    //   cwd: '/var/www/vhosts/1usdt.game/blockchain-network',
+    //   script: 'tron.js',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //   },
+    //   instances: 1,
+    //   autorestart: true,
+    //   max_memory_restart: '512M',
+    // },
   ],
 };
